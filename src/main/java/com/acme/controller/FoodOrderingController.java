dcb5575739befaffb3ecb602573b01840bce5e31
brunordg <rodrigues.brodrigues@gmail.com>
2022-09-21 23:48:44 -0300

    feat(finish): Finalizando a primeira aplicação com event sourcing e CQRS com a utilização do framwork axon

--- src/main/java/com/acme/controller/FoodOrderingController.java
+++ src/main/java/com/acme/controller/FoodOrderingController.java
@@ -2,8 +2,11 @@ package com.acme.gui;
 
 import com.acme.coreapi.CreateFoodCartCommand;
 import com.acme.coreapi.DeselectProductCommand;
+import com.acme.coreapi.FindFoodCartQuery;
 import com.acme.coreapi.SelectProductCommand;
+import com.acme.query.FoodCartView;
 import org.axonframework.commandhandling.gateway.CommandGateway;
+import org.axonframework.messaging.responsetypes.ResponseTypes;
 import org.axonframework.queryhandling.QueryGateway;
 import org.springframework.web.bind.annotation.*;
 
@@ -38,13 +41,15 @@ public class FoodOrderingController {
 
     @PostMapping("/{foodCartId}/deselect/{productId}/quantity/{quantity}")
     public void deselecProduct(@PathVariable("foodCartId") String foodCartId,
-                             @PathVariable("productId") String productId,
-                             @PathVariable("quantity") Integer quantity) {
+                               @PathVariable("productId") String productId,
+                               @PathVariable("quantity") Integer quantity) {
         this.commandGateway.send(new DeselectProductCommand(UUID.fromString(foodCartId), UUID.fromString(productId), quantity));
     }
 
-//    @GetMapping("/{foodCartId}")
-//    public CompletableFuture<> findFoodCart(@PathVariable("foodCartId") String foodCartId) {
-//        this.queryGateway.query();
-//    }
+    @GetMapping("/{foodCartId}")
+    public CompletableFuture<FoodCartView> findFoodCart(@PathVariable("foodCartId") String foodCartId) {
+        return queryGateway.query(
+                new FindFoodCartQuery(UUID.fromString(foodCartId)), ResponseTypes.instanceOf(FoodCartView.class)
+        );
+    }
 }
